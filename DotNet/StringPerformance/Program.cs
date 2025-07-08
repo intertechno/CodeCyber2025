2ef4493eac5afa1120372d19951e5676acf1492e
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-07-08 15:53:35 +0300

    Improved the performance of the C# string processing logic by utilitizing a StringBuilder.

--- DotNet/StringPerformance/Program.cs
+++ DotNet/StringPerformance/Program.cs
@@ -1,11 +1,23 @@
-﻿
+﻿using System.Text;
+
 Console.WriteLine("Starting to create a long string...");
 DateTime start = DateTime.Now;
+
+StringBuilder builder = new();
+for (int i = 0; i < 500_000; i++)
+{
+    builder.Append("A");
+}
+string s = builder.ToString();
+
+/*
 string s = "";
 for (int i = 0; i < 600_000; i++)
 {
     s += "A";
 }
+*/
+
 DateTime end = DateTime.Now;
 Console.WriteLine("String created.");
 
