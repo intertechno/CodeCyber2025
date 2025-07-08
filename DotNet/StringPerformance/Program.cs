df7c8731b41c991b904744eb2ffb2ec3e0088110
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-07-08 15:16:40 +0300

    Added C# example on string processing performance.

---
+++ DotNet/StringPerformance/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Starting to create a long string...");
+DateTime start = DateTime.Now;
+string s = "";
+for (int i = 0; i < 600_000; i++)
+{
+    s += "A";
+}
+DateTime end = DateTime.Now;
+Console.WriteLine("String created.");
+
+TimeSpan amount = (end - start);
+double seconds = amount.TotalSeconds;
+System.Console.WriteLine($"The operation took {seconds} seconds.");
