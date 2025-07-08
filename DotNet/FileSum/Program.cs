963a5984b2d1884c681e92ff63ced1ae86f066ff
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-07-08 13:58:00 +0300

    Added three C# example applications: Hello World, file sum and number guessing game.

---
+++ DotNet/FileSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+const string InputFile = @"C:\Academy\Code Cyber\Code\DotNet\Numbers.txt";
+string[] lines = [.. File.ReadLines(InputFile)];
+
+// part 1: calculating sum and average
+int sum = 0;
+foreach (string line in lines)
+{
+    int value = int.Parse(line);
+    sum += value;
+}
+
+double average = sum / lines.Length;
+Console.WriteLine($"Calculated file numbers, sum is {sum}, and average {average}.");
+
+// part 2: store results in JSON into a file
+string json = JsonSerializer.Serialize(new { sum, average });
+File.WriteAllText("Result.json", json);
+Console.WriteLine("Wrote JSON result file.");
---
+++ DotNet/NumberGuessingGame/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Welcome to the number guessing game!");
+
+int correct = Random.Shared.Next(1, 21);
+// Console.WriteLine(correct);
+
+for (int attempt = 0; attempt < 3; attempt++)
+{
+    Console.WriteLine("Please enter your guess (1-20):");
+    string input = Console.ReadLine() ?? "0";
+    int guess = int.Parse(input);
+
+    if (guess == correct)
+    {
+        Console.WriteLine("Correct guess! You win the game.");
+        break;
+    }
+    else if (guess < correct)
+    {
+        Console.WriteLine("Not quite! The correct number is larger.");
+    }
+    else if (guess > correct)
+    {
+        Console.WriteLine("Not quite! The correct number is smaller.");
+    }
+}
+
+Console.WriteLine("The game ends.");
