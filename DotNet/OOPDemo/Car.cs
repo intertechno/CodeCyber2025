c0c79a441b68305f23f62198724b76c393aebb24
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-07-23 10:45:44 +0300

    Started to develop a simple OOP demo application using C#.

---
+++ DotNet/OOPDemo/Car.cs
@@ -0,0 +1,47 @@
+class Car
+{
+    // properties
+    public string Model { get; init; } = "";
+    public string Make { get; init; } = "";
+    public int CurrentSpeed { get; set; }
+    public bool EngineRunning { get; set; }
+
+    // methods
+    public void StartEngine()
+    {
+        EngineRunning = true;
+        Console.WriteLine("Engine started.");
+    }
+
+    public void StopEngine()
+    {
+        EngineRunning = false;
+        Console.WriteLine("Engine stopped.");
+    }
+
+    public void Accelerate(int speed)
+    {
+        if (EngineRunning)
+        {
+            CurrentSpeed += speed;
+            Console.WriteLine($"Accelerated to {CurrentSpeed} km/h.");
+        }
+        else
+        {
+            Console.WriteLine("Cannot accelerate, the engine is not running.");
+        }
+    }
+
+    public void Decelerate(int speed)
+    {
+        if (CurrentSpeed >= speed)
+        {
+            CurrentSpeed -= speed;
+            Console.WriteLine($"Slowed down to {CurrentSpeed} km/h.");
+        }
+        else
+        {
+            Console.WriteLine("Cannot decelerate, speed is too low.");
+        }
+    }
+}
---
+++ DotNet/OOPDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Welcome to the OOP Demo!");
+
+/*
+// creating an instance of the Car class (old-school way)
+Car myCar = new Car();
+myCar.Make = "Toyota";
+myCar.Model = "Camry";
+myCar.CurrentSpeed = 0;
+myCar.EngineRunning = false;
+*/
+
+// var myCar2 = new Car();
+Car myCar = new()
+{
+    Make = "Toyota",
+    Model = "Camry"
+};
