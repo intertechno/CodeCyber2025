978aff2be86500c8e36a1a3c881dfe167d59d551
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-07-24 15:38:43 +0300

    Added NuGet packages for SQL Server with Entity Framework and added simple code to list Finnish customers.

--- DotNet/EfDemo2/EfDemo2/Program.cs
+++ DotNet/EfDemo2/EfDemo2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EfDemo2.Models;
+
+Console.WriteLine("Welcome to the EF Demo 2!");
+
+using NorthwindContext context = new();
+List<Customer> finnishCustomers =
+    [.. context.Customers.Where(c => c.Country == "Finland")];
+
+foreach (Customer customer in finnishCustomers)
+{
+    Console.WriteLine($"{customer.CustomerId} {customer.CompanyName}");
+}
