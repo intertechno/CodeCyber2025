fa1f589ad0a2ab31ebdbfd7fc1c44a43d2c3150a
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-07-24 11:26:14 +0300

    Added a simple C# application to access a SQLite sample Northwind database using ADO.NET.

---
+++ DotNet/SQLiteDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SQLite;
+
+Console.WriteLine("Welcome to the SQLite Example!");
+
+string connectionString = @"Data Source=C:\Users\Koulutus\Desktop\northwind.db;Version=3;";
+using SQLiteConnection connection = new(connectionString);
+connection.Open();
+Console.WriteLine("Connection to SQLite established.");
+
+string sql = """
+    SELECT *
+    FROM Customers
+    WHERE Country = "Finland"
+    """;
+
+// execute the SQL query and display the results
+using SQLiteCommand command = new(sql, connection);
+using SQLiteDataReader reader = command.ExecuteReader();
+while (reader.Read())
+{
+    Console.WriteLine($"Customer ID: {reader["CustomerID"]}, Name: {reader["CompanyName"]}");
+}
+
+Console.WriteLine("Query executed successfully.");
