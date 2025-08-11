376b6633d55ad4875caa866ae79b377fcacfd855
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-08-11 14:32:53 +0300

    Added a simple API controller that uses EF to access the Northwind database and return a list of customers.

---
+++ DotNet/AspNetNorthwindWebApi/AspNetNorthwindWebApi/Controllers/CustomersApiController.cs
@@ -0,0 +1,18 @@
+﻿using AspNetNorthwindWebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetNorthwindWebApi.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomersApiController : ControllerBase
+    {
+        public List<Customer> GetAllCustomers()
+        {
+            using NorthwindContext context = new();
+            List<Customer> customers = [.. context.Customers];
+            return customers;
+        }
+    }
+}
