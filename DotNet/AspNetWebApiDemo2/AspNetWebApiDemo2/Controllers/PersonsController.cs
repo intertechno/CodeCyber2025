9d09028f6292b03d320365d2703ddabb94de11f3
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-08-11 11:36:09 +0300

    Added a second ASP.NET Core Web API demo with support for reading CSV file data and showing that data in JSON array format.

---
+++ DotNet/AspNetWebApiDemo2/AspNetWebApiDemo2/Controllers/PersonsController.cs
@@ -0,0 +1,40 @@
+﻿using AspNetWebApiDemo2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetWebApiDemo2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        public List<PersonModel> GetPersons()
+        {
+            // read the CSV file data
+            string csvPath = @"C:\Academy\Code Cyber\Code\Persons.csv";
+            string[] csvLines = System.IO.File.ReadAllLines(csvPath);
+
+            // convert the CSV data into PersonModel objects
+            List<PersonModel> persons = [];
+            bool isFirstLine = true;
+            foreach (string line in csvLines)
+            {
+                if (isFirstLine)
+                {
+                    isFirstLine = false;
+                    continue;
+                }
+
+                string[] parts = line.Split(',');
+                persons.Add(new PersonModel
+                {
+                    PersonNumber = int.Parse(parts[0]),
+                    PersonName = parts[1],
+                    Birthdate = parts[2]
+                });
+            }
+
+            return persons;
+        }
+    }
+}
---
+++ DotNet/AspNetWebApiDemo2/AspNetWebApiDemo2/Models/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetWebApiDemo2.Models
+{
+    public class PersonModel
+    {
+        public int PersonNumber { get; set; }
+
+        public string PersonName { get; set; }
+
+        public string Birthdate { get; set; }
+    }
+}
