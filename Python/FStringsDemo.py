f4ee2775d8337f16b1d248bfc649860b8d5930c6
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-08-27 13:19:31 +0300

    Continued developing the number frequencies demo application. Also added a quick demo on F-strings.

---
+++ Python/FStringsDemo.py
@@ -0,0 +1,4 @@
+num = 123
+print(f"The number is: {num}")
+
+print(f"Hello World! { 1 + 1 }")
--- Python/FrequenciesOfNumbers.py
+++ Python/FrequenciesOfNumbers.py
@@ -1,8 +1,17 @@
 
-number = [5, 23, 33, 17]
+numbers = [5, 23, 33, 17, 3, 15]
+freq = {}
 
-for n in number:
+for n in numbers:
     bracket = n // 10 * 10
     bracket_str = f"{bracket}-{bracket + 9}"
+    if bracket_str not in freq:
+        freq[bracket_str] = 1
+    else:
+        freq[bracket_str] += 1
 
     print(f"Number {n} is in the bracket {bracket_str}")
+    # print("Number ", n, " is in the bracket ", bracket_str)
+
+print("------------------")
+print(freq)
