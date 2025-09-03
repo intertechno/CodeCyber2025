a9dc73837ec7f7c18f4c8b3eee77ecef1c1b5767
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-09-03 09:31:57 +0300

    Added simple math examples using Python.

---
+++ Python/MathTest.py
@@ -0,0 +1,12 @@
+import math
+import MathHelper
+
+print(MathHelper.add(5, 3))
+MathHelper.subtract(5, 3)
+MathHelper.multiply(5, 3)
+MathHelper.divide(5, 3)
+
+# ------------------
+
+number = 25
+print(math.sqrt(number))
