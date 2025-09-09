7052e6075538c69dab4e46485a87034f1078f87b
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-09-09 15:49:05 +0300

    Changed the color theme to use black and red instead of blue and red.

--- Python/PandasCsvAndSeabornHeatmapWithMatplotlib.py
+++ Python/PandasCsvAndSeabornHeatmapWithMatplotlib.py
@@ -10,11 +10,12 @@ pivot_df = df.pivot(index="year", columns="month", values="avg_temp_c")
 
 # 3. Draw heatmap
 plt.figure(figsize=(10, 6))
+sns.set_theme()
 sns.heatmap(
     pivot_df,
     annot=True,        # show temperature values
     fmt=".1f",         # one decimal place
-    cmap="coolwarm",   # blue = cold, red = hot
+    # cmap="coolwarm",   # blue = cold, red = hot
     cbar_kws={'label': 'Avg Temp (°C)'}
 )
 
