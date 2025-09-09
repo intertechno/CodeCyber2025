496c861b55bc1ac85f9e0417f79e23f5374e037b
Jani Järvinen <jani.jarvinen@intertechno.org>
2025-09-09 14:08:08 +0300

    Added an "oneliner" example on filtering world cities based on population.

--- Python/PandasAndCsvWorldCities.py
+++ Python/PandasAndCsvWorldCities.py
@@ -11,3 +11,8 @@ step1 = cities[cities["population"] >= 500000]
 step2 = step1[step1["population"] <= 1000000]
 
 print(step2.sort_values(["population"], ascending=False))
+
+# ------- "oneliner" --------
+print("-------------")
+filtered = cities[cities["population"].between(500000, 1000000)]
+print(filtered.sort_values(["population"], ascending=False))
